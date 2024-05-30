6164c6f39e95194c739ad2a9c05843e621257a63
Aditi Kumari <aditisingh49893@gmail.com>
2024-05-30 23:07:50 +0530

    Project server setup

---
+++ server/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+export const app = express();  
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body Parser
+app.use(express.json({limit:"50mb"}));
+
+//cookie-parser
+app.use(cookieParser());
+
+//cors => cross origin resource sharing
+
+app.use(cors({}));
