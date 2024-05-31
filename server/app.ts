2456c6d876ae3d254c6a76f82fed66a259aaec34
Aditi Kumari <aditisingh49893@gmail.com>
2024-06-01 00:08:56 +0530

    User model design

--- server/app.ts
+++ server/app.ts
@@ -1,39 +1,39 @@
-require('dotenv').config();
-import express,{NextFunction, Request,Response} from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 
-export const app = express();  
+export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {ErrorMiddleware} from "./middlewares/error";
+import { ErrorMiddleware } from "./middlewares/error";
 
 //body Parser
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 //cookie-parser
 app.use(cookieParser());
 
 //cors => cross origin resource sharing
 
-app.use(cors({
-    origin: process.env.ORIGIN
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 //testing api
 
-app.get("/test", (req:Request, res:Response,next:NextFunction) => {
-res.status(200).json({
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
     success: true,
-    message: "Test successful"
-});
+    message: "Test successful",
+  });
 });
 
 //unknown route
-app.all("*",(req:Request, res:Response, next:NextFunction) => {
-    const err = new Error(`Route ${req.originalUrl} not found`) as any;
-    err.statusCode=400;
-    next(err);
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 400;
+  next(err);
 });
 
-
 app.use(ErrorMiddleware);
-
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
-import {app} from "./app";
+import { app } from "./app";
 import connectDB from "./utils/db";
-require ("dotenv").config();
+require("dotenv").config();
 
 // create server
-app.listen( process.env.PORT,()=>{
-    console.log(`server is connected with port ${process.env.PORT}`)
-    connectDB();
+app.listen(process.env.PORT, () => {
+  console.log(`server is connected with port ${process.env.PORT}`);
+  connectDB();
 });
